75ad19b2e5157600e7a238e02ded9ee05b8c8c5a
EnzoSantoss <enzoaugusto312@gmail.com>
2024-03-11 22:04:22 -0300

    experiences

---
+++ src/app/modules/portifolio/components/experiences/experiences.component.ts
@@ -0,0 +1,34 @@
+import { Component, signal } from '@angular/core';
+import { IExperience } from '../../interface/IExperience.inteface';
+
+@Component({
+  selector: 'app-experiences',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiences.component.html',
+  styleUrl: './experiences.component.scss',
+})
+export class ExperiencesComponent {
+  public experiencesArray = signal<IExperience[]>([
+    {
+      summary: {
+        strong: 'Desenvolvedor de Software Estagiário',
+        p: 'AnyGrid Solar Service | 2022 - 2023',
+      },
+      text: `Encarregado do desenvolvimento e manutenção
+      de softwares internos vitais, primordialmente
+      utilizando Node.js e TypeScript.`,
+    },
+    {
+      summary: {
+        strong: 'Desenvolvedor de Software Júnior',
+        p: 'Tropa Digital | 2023 - Atualmente',
+      },
+      text: `Responsável por criar sistemas escaláveis e
+      seguros utilizando Node.js, com ênfase no
+      framework Nest.js e MySQL. Especializado em
+      construir APIs robustas e eficientes, focadas na
+      entrega de alto desempenho e confiabilidade.`,
+    },
+  ]);
+}
